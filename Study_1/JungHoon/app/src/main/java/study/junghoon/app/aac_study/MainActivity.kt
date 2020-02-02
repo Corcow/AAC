49936728abae0e71a3143bab78fffd25a5ce496c
parkjunghoon <go9018@naver.com>
2020-02-02 14:53:13 +0900

    add : gradle setting

--- Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/MainActivity.kt
+++ Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/MainActivity.kt
@@ -1,12 +1,17 @@
 package study.junghoon.app.aac_study
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    /*https://codelabs.developers.google.com/codelabs/android-databinding/#0
+    codelab 에 있는 DataBinding 예제를 구현해보자
+    * */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
