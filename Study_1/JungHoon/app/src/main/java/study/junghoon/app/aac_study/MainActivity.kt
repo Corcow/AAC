5d3b2b2dd3a23c53f42790d720fcc6aca674ed49
parkjunghoon <go9018@naver.com>
2020-02-02 15:04:19 +0900

    add : plain activity + xml & xml values

--- Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/MainActivity.kt
+++ Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/MainActivity.kt
@@ -2,16 +2,21 @@ package study.junghoon.app.aac_study
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     /*https://codelabs.developers.google.com/codelabs/android-databinding/#0
-    codelab 에 있는 DataBinding 예제를 구현해보자
+    * codelab 에 있는 DataBinding 예제를 구현해보자
     * */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        go_to_plain_activity.setOnClickListener {
+
+        }
 
     }
 }
---
+++ Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/plain/PlainActivity.kt
@@ -0,0 +1,19 @@
+package study.junghoon.app.aac_study.plain
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import study.junghoon.app.aac_study.R
+
+class PlainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plain)
+    }
+
+    // Like button onClick
+    fun onLike(view: View) {
+
+    }
+}
